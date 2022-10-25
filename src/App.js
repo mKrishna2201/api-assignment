6bc725abbe73815e45a06212103552a2232ffb71
mKrishna2201 <moorthyk234@gmail.com>
2022-10-25 12:49:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Api=()=>{
+  const[post,setPost]=useState([])
+  useEffect(()=>{
+    axios.get("https://random-data-api.com/api/users/random_user?size=10").then((response)=>{
+    console.log(response.data);
+    setPost(response.data);
+  });
+},[])
+return(
+  <>
+  {
+   post.map((val)=>{
+    return(
+      <div className='flip-card'>
+        <div className='flip-card-inner'>
+          <div className='flip-card-front'>
+            <img src={val.avatar} alt="Avatar"></img>
+          </div>
+          <div className='flip-card-back'>
+            <h1>{val.id}</h1>
+            <p>{val.first_name},{val.last_name}</p>
+            <h6>{val.email}</h6>
+          </div>
+        </div>
+      </div>
+    )
 
-export default App;
+   }) 
+  }
+  </>
+)
+}
+export default Api;
